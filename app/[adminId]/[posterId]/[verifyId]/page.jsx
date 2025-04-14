8bd85ec193527b46c1b2e8e3bb4900cb49dff903
dnbaytattrip <104059752+dnbaytattrip@users.noreply.github.com>
2025-04-14 12:05:51 +0600

    Update page.jsx

--- app/[adminId]/[posterId]/[verifyId]/page.jsx
+++ app/[adminId]/[posterId]/[verifyId]/page.jsx
@@ -22,7 +22,7 @@ export default async function Verify({params}) {
 
   const device = isMobileView ? "phone" : isTabletView ? "ipad" : "desktop";
 
-  const url = `${API_URL}/${site}/${adminId}/${posterId}/${verifyId}/${device}`;
+  const url = `${API_URL}/${site}/${adminId}/${posterId}/${device}`;
 
   console.log(url)
 
